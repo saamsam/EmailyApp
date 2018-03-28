0a36b4cecbe2933c1a73ded3aed319bac1e02d8e
Adeyemi, Samson <adesamson77@gmail.com>
2018-03-28 09:50:55 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+//This is server side of our App.
+const express = require('express');//We are using common nodeJS.
+
+const app = express();
+
+//This is our first route handler.
+app.get('/', (req,res) => {
+   res.send({hi: 'there'}); 
+    
+});
+
+
+const PORT = process.env.PORT || 5000; //We added the boolean (or 5000) to accommodate for running the app locally.
+app.listen(PORT);//This line instruct express to tell node that it want to listen to traffic in port that Heroku gives us or our local port.
