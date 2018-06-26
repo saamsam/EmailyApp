f79889fc6f028d1e70b20c6c75c0af847a39c466
Adeyemi, Samson <adesamson77@gmail.com>
2018-06-25 21:34:46 -0500

    random changes

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -6,7 +6,7 @@ import DateTime from './DateTime';
 const Landing = () => {
   return (
     <div style={{ textAlign: 'center' }}>
-      <h1>SamTech Inc.!</h1>
+      <h1>Building Better Apps!</h1>
       Welcome, We Are Glad You Are Here!
       <div className="carousel-fixed-item center">
         <img src={Horse} />
